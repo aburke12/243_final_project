a4cbdf8b6da2ac651d72c3e6c9a23bc18b91e38e
unknown <burkeally778@gmail.com>
2018-11-30 09:44:29 -0500

    Updated hapi-server

--- hapi-server.js
+++ hapi-server.js
@@ -1,2 +1,31 @@
 // Server Page
 // Authors: Ally Burke, Lauren James
+
+// Standard Node modules
+const Path = require('path');
+
+// Knex
+const knex = require("knex")({
+    client: "pg",
+    connection: {
+        host: "faraday.cse.taylor.edu",
+        database: "ally_burke",
+	user: "ally_burke",
+	password: "wadurahu"
+    }
+});
+
+// hapi
+const Boom = require('boom');
+const Joi = require('joi');
+const Hapi = require('hapi');
+
+const server = Hapi.server({
+  host: 'localhost',
+  port: 3000,
+  routes: {
+    files: {
+      relativeTo: Path.join(__dirname, 'public')
+    }
+  }
+});
